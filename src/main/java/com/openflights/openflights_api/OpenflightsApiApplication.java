f2f89e391fd15e6fdd1fb78d8c6123931f2f9bd8
Lage <davilaggee@gmail.com>
2025-12-08 20:13:58 -0300

    feat: commit inicial do springboot

---
+++ src/main/java/com/openflights/openflights_api/OpenflightsApiApplication.java
@@ -0,0 +1,13 @@
+package com.openflights.openflights_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenflightsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenflightsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openflights/openflights_api/OpenflightsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openflights.openflights_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenflightsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
