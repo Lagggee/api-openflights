7a39ce7958acdc277796695055641ed1fed63333
Lage <davilaggee@gmail.com>
2025-12-08 23:02:44 -0300

    feat: implementação da exception personalizada

---
+++ src/main/java/com/openflights/openflights_api/Exceptions/AeroportoNaoEncontradoException.java
@@ -0,0 +1,12 @@
+package com.openflights.openflights_api.Exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AeroportoNaoEncontradoException extends RuntimeException {
+
+    public AeroportoNaoEncontradoException(String iata) {
+        super("Aeroporto com código IATA '" + iata + "' não encontrado.");
+    }
+}
